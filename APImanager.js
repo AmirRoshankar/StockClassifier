3875075720a517c9a0e46e04aff34e0daafe53ba
Mingye <mingyechen521@gmail.com>
2020-09-19 23:23:29 -0400

    Add function get_all_data to asynchronously call and fetch api data

--- APImanager.js
+++ APImanager.js
@@ -1,4 +1,4 @@
-"using strict"
+"use strict"
 const fs = require('fs');
 const fetch = require('node-fetch');
 
@@ -6,26 +6,26 @@ const fetch = require('node-fetch');
 let rawjson = fs.readFileSync('APIkey.json');
 let mykey = JSON.parse(rawjson);
 
+let listOfCompanies;
+
 // Fetch S&P companies
-function get_SandP_companies(){
+async function get_SandP_companies(){
     let url = 'https://finnhub.io/api/v1/index/constituents?symbol=^GSPC&token='+ mykey.key;
-    let companies;
-    fetch(url)
-    .then(res => res.json())
-    .then(json => {
-        console.log(json);
-        // Save to a json file
-        companies = json;
-        fs.writeFile('SandP500companies.json', JSON.stringify(json), (err)=>{
-            if(err) throw err;
-            console.log('saved');
-        });
-    })
-    .catch(err => {
+    console.log("Fetching S&P 500 companies");
+    try{
+        let response = await fetch(url);
+        listOfCompanies = await response.json();
+    }
+    catch(err){
         console.log(err);
-    });
+        return;
+    }
+
 }
 
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 async function get_full_OHLC_history(symbol){
     // Get current time and a year ago in Epoch format
     let currentTime = Math.floor(+new Date() / 1000);
@@ -43,9 +43,21 @@ async function get_full_OHLC_history(symbol){
         console.log(err);
         return;
     }
+    console.log("Data for : " + symbol);
     console.log(ohlcData);
+    // WRITE TO DATABASE COMING SOON
 }
 
-// Test function with 3M
-get_full_OHLC_history("MMM");
+// Basically a main()
+async function get_all_data(){
+    await get_SandP_companies();
+    // Issue with the api call quota. 60 does not seem to be correct
+    for(let i = 0; i < 40; i++){
+        console.log("fetching for company No." + (i+1) + " : " + listOfCompanies.constituents[i]);
+        get_full_OHLC_history(String(listOfCompanies.constituents[i]));
+    }
+}
 
+get_all_data();
+//test for individual case
+//get_full_OHLC_history("MMM");
